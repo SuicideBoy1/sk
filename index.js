2eb21e157310dcbb156db3902912944730d2ce59
SuicideBoy1 <42441176+SuicideBoy1@users.noreply.github.com>
2018-08-27 19:38:14 +0400

    Update index.js

--- index.js
+++ index.js
@@ -92,6 +92,21 @@ if (message.content.startsWith("!avatar")){
     message.channel.send({embed});
 }
 
+if (message.content.startWith("!color")){
+  var color = args[0]
+  if(!color) return msg.reply('Ты должен указать цвет')
+  msg.member.roles.forEach(role => {
+    if(role.name.startsWith('✬ ')) {
+      if(role.members.size == 1) role.delete()
+      else msg.member.removeRole(role)
+    }
+  })
+  var role = msg.guild.roles.find('name', `✬ ${color} ✬`)
+  if(!role) role = await msg.guild.createRole({name: `✬ ${color}`, color})
+  msg.member.addRole(role)
+  msg.channel.send('Готово!')
+}
+
 
     if (message.member.hasPermission('MANAGE_GUILD') || message.member.hasPermission('ADMINISTRATOR') || message.member.id === message.guild.owner.id) {
         if (message.content === '!stay') {stop.push(message.guild.id); return message.channel.send('Готово');}
